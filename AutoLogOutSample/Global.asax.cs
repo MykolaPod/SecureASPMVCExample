ef80a3df2ae504f3c4c1159170444afe04926e02
MykolaPo <npodoprygora@ego-cms.com>
2017-02-20 16:09:12 +0200

    fix new session id per request issue

--- AutoLogOutSample/Global.asax.cs
+++ AutoLogOutSample/Global.asax.cs
@@ -1,7 +1,13 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Net;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using AutoLogOutSample.Services;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
 
 namespace AutoLogOutSample
 {
@@ -20,6 +26,18 @@ namespace AutoLogOutSample
         protected void Application_End()
         {
             WebBackgrounderSetup.Shutdown();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var authenticationManager = HttpContext.Current.GetOwinContext().Authentication;
+
+                authenticationManager.SignOut(DefaultAuthenticationTypes.ApplicationCookie);
+
+                HttpContext.Current.User = new GenericPrincipal(new GenericIdentity(string.Empty), null);
+            }
         }
     }
 }
