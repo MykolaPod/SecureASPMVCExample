5b328e341cbb0ef4ee4d1b9f2cee89c8fd4fc91d
MykolaPo <npodoprygora@ego-cms.com>
2017-02-20 16:06:50 +0200

    added filters

--- AutoLogOutSample/App_Start/FilterConfig.cs
+++ AutoLogOutSample/App_Start/FilterConfig.cs
@@ -1,5 +1,6 @@
 ﻿using System.Web;
 using System.Web.Mvc;
+using AutoLogOutSample.Attributes;
 
 namespace AutoLogOutSample
 {
@@ -7,6 +8,8 @@ namespace AutoLogOutSample
     {
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
+            filters.Add(new NoCacheAttribute());
+            filters.Add(new AuthorizeAttribute());
             filters.Add(new HandleErrorAttribute());
         }
     }
---
+++ AutoLogOutSample/Attributes/NoCacheAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AutoLogOutSample.Attributes
+{
+    [AttributeUsage(AttributeTargets.All)]
+    public class NoCacheAttribute : ActionFilterAttribute
+    {
+        public override void OnResultExecuting(ResultExecutingContext filterContext)
+        {
+            try
+            {
+                filterContext.HttpContext.Response.Cache.SetExpires(DateTime.UtcNow.AddDays(-1));
+                filterContext.HttpContext.Response.Cache.SetValidUntilExpires(false);
+                filterContext.HttpContext.Response.Cache.SetRevalidation(HttpCacheRevalidation.AllCaches);
+                filterContext.HttpContext.Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                filterContext.HttpContext.Response.Cache.SetNoStore();
+                filterContext.HttpContext.Response.Cache.SetMaxAge(TimeSpan.Zero);
+                filterContext.HttpContext.Response.Cache.SetVaryByCustom("*");
+            }
+            catch (Exception e)
+            {
+                //nothing
+            }
+
+            base.OnResultExecuting(filterContext);
+        }
+    }
+}
